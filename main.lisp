d2fe4e0fc9de90d1fcfd028f5c839eebdaefc64d
t-sin <shinichi.tanaka45@gmail.com>
2023-10-07 22:28:52 +0900

    Introduce `test` macro that runs tests immediately

--- main.lisp
+++ main.lisp
@@ -1,50 +1,20 @@
 (defpackage :ethogram
   (:use :cl)
-  (:export :<result>
-           :result-succeeded?
-           :result-expected
-           :result-actual
-
-           :<context>
-           :context-description
-           :context-result))
+  (:export))
 (in-package :ethogram)
 
-(defclass <result> ()
-  ((succeeded? :initarg :succeeded?
-              :initform nil
-              :reader result-succeeded?)
-   (expected :initarg :expected
-             :initform nil
-             :reader result-expected)
-   (actual :initarg :actual
-           :initform nil
-           :reader result-actual)))
-
-(defgeneric diff (result))
-
-(defclass <context> ()
-  ((description :initarg :subject
-                :initform nil
-                :type string
-                :reader context-description)
-   (result :initarg :result
-           :initform ()
-           :reader context-result)))
-
-(defgeneric parse (context definition))
-(defgeneric validate (context))
-
-(defun dispatch-context (context-name))
-
-(defclass <group-context> (<context>))
-(defclass <test-context> (<context>))
-(defgeneric examine (context))
-
-(defmacro test ())
-
-(defclass <function> (<context>)
-  ((subject :type function))
-  ((inputs :initarg :inputs
-           :initform ()
-           :accessor funtion-inputs)))
+(defmacro test (context &body dsl)
+  (cond ((eq context :function)
+         (let ((subject (first dsl))
+               (rest (rest dsl)))
+           (when (or (functionp subject) (symbolp subject))
+             (error "not a function: ~s" subject))
+           (alexandria:with-gensyms (%subject)
+             `(let ((,%subject ,subject))
+                ,@(loop
+                    :for (k$for input k$returns actual) :on rest by #'cddddr
+                    :collect (let ((args (if (atom input)
+                                             (list input)
+                                             input)))
+                               `(equalp (apply ,%subject ',args) ,actual)))))))
+        (t (error "a test context ~s is not implemented yet" context))))
