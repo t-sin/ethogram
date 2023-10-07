748cd9592f232275094b7945def80d46cf46b18c
t-sin <shinichi.tanaka45@gmail.com>
2023-10-07 23:56:27 +0900

    Implement basic testing process

--- ethogram.asd
+++ ethogram.asd
@@ -3,5 +3,5 @@
   :version "0.0.1"
   :author "t-sin <shinichi.tanaka45@gmai.com>"
   :license "currently non-licensed"
-  :depends-on ()
+  :depends-on ("alexandria")
   :components ((:file "main")))
--- main.lisp
+++ main.lisp
@@ -3,18 +3,54 @@
   (:export))
 (in-package :ethogram)
 
+(defstruct testcase
+  input expected actual result)
+
 (defmacro test (context &body dsl)
   (cond ((eq context :function)
          (let ((subject (first dsl))
                (rest (rest dsl)))
            (when (or (functionp subject) (symbolp subject))
              (error "not a function: ~s" subject))
-           (alexandria:with-gensyms (%subject)
-             `(let ((,%subject ,subject))
-                ,@(loop
-                    :for (k$for input k$returns actual) :on rest by #'cddddr
-                    :collect (let ((args (if (atom input)
-                                             (list input)
-                                             input)))
-                               `(equalp (apply ,%subject ',args) ,actual)))))))
+           (let* ((testdefs (loop
+                              :for test := (subseq rest 0 4)
+                              :for (k$for input k$returns expected) :on (rest dsl) by #'cddddr
+                              :unless (and (eq k$for :for) (eq k$returns :returns))
+                              :do (error "a mulformed test: ~s" test)
+                              :collect (list :input input :expected expected)))
+                  (num-of-defs (length testdefs)))
+             (setf testdefs (nreverse testdefs))
+             (alexandria:with-gensyms (%subject %testdefs %testdef %testcases
+                                                %n %testcase %input %actual %failed)
+               `(let ((,%subject ,subject)
+                      (,%testdefs ',testdefs)
+                      (,%testcases (make-array ,num-of-defs))
+                      (,%failed ()))
+                  ;; initializing
+                  (loop
+                    :for ,%n :from 0 :below ,num-of-defs
+                    :for ,%testdef := (nth ,%n ,%testdefs)
+                    :do (setf (aref ,%testcases ,%n)
+                              (make-testcase :input (getf ,%testdef :input)
+                                             :expected (getf ,%testdef :expected))))
+                  ;; running tests
+                  (loop
+                    :for ,%n :from 0 :below ,num-of-defs
+                    :for ,%testcase := (aref ,%testcases ,%n)
+                    :for ,%input := (let ((,%input (testcase-input ,%testcase)))
+                                      (if (atom ,%input)
+                                          (list ,%input)
+                                          ,%input))
+                    :for ,%actual := (apply ,%subject ,%input)
+                    :do (setf (testcase-actual ,%testcase)
+                              ,%actual)
+                    :do (setf (testcase-result ,%testcase)
+                              (equalp ,%actual (testcase-expected ,%testcase))))
+                  ;; collect results
+                  (loop
+                    :for ,%n :from 0 :below ,num-of-defs
+                    :for ,%testcase := (aref ,%testcases ,%n)
+                    :unless (testcase-result ,%testcase)
+                    :do (push ,%testcase ,%failed))
+                  ,%testcases)))))
         (t (error "a test context ~s is not implemented yet" context))))
