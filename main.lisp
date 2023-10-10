cabe4ee66e24a7a7c3f63301ceef9c79819a64cd
t-sin <shinichi.tanaka45@gmail.com>
2023-10-10 21:58:17 +0900

    Split codes in `test` into some functions

--- main.lisp
+++ main.lisp
@@ -4,53 +4,66 @@
 (in-package :ethogram)
 
 (defstruct testcase
-  input expected actual result)
+  name
+  input expected actual succeeded-p)
+
+(defun valid-context-p (subject)
+  (when (or (functionp subject) (symbolp subject))
+    (values nil (format nil "not a function: ~s" subject)))
+  t)
+
+(defun parse-defs (sexp)
+  (let ((defs (loop
+                :for (k$for input k$returns expected) :on sexp by #'cddddr
+                :unless (and (eq k$for :for) (eq k$returns :returns))
+                :do (error "a mulformed test: ~s" (list k$for input k$returns expected))
+                :collect (list :input input :expected expected))))
+    (nreverse defs)))
+
+(defun prepare-testcases (defs)
+  (let ((testcases (make-array (length defs))))
+    (loop
+      :for n :from 0 :below (length defs)
+      :for def := (elt defs n)
+      :do (setf (elt testcases n)
+                (make-testcase :input (getf def :input)
+                               :expected (getf def :expected))))
+    testcases))
+
+(defun run-testcases (subject testcases)
+  (loop
+    :for n :from 0 :below (length testcases)
+    :for testcase := (elt testcases n)
+    :for input := (let ((input (testcase-input testcase)))
+                    (if (atom input)
+                        (list input)
+                        input))
+    :for actual := (apply subject input)
+    :do (setf (testcase-actual testcase) actual)
+    :do (let ((expected (testcase-expected testcase)))
+          (setf (testcase-succeeded-p testcase)
+                (equalp actual expected)))))
+
+(defun collect-result (testcases)
+  (let ((failed ()))
+    (loop
+      :for n :from 0 :below (length testcases)
+      :for testcase := (elt testcases n)
+      :unless (testcase-succeeded-p testcase)
+      :do (push testcase failed))
+    (nreverse failed)))
 
 (defmacro test (context &body dsl)
   (cond ((eq context :function)
          (let ((subject (first dsl))
                (rest (rest dsl)))
-           (when (or (functionp subject) (symbolp subject))
-             (error "not a function: ~s" subject))
-           (let* ((testdefs (loop
-                              :for test := (subseq rest 0 4)
-                              :for (k$for input k$returns expected) :on (rest dsl) by #'cddddr
-                              :unless (and (eq k$for :for) (eq k$returns :returns))
-                              :do (error "a mulformed test: ~s" test)
-                              :collect (list :input input :expected expected)))
-                  (num-of-defs (length testdefs)))
-             (setf testdefs (nreverse testdefs))
-             (alexandria:with-gensyms (%subject %testdefs %testdef %testcases
-                                                %n %testcase %input %actual %failed)
+           (multiple-value-bind (validp msg)
+               (valid-context-p subject)
+             (unless validp (error msg)))
+           (let ((testdefs (parse-defs rest)))
+             (alexandria:with-gensyms (%subject %testcases)
                `(let ((,%subject ,subject)
-                      (,%testdefs ',testdefs)
-                      (,%testcases (make-array ,num-of-defs))
-                      (,%failed ()))
-                  ;; initializing
-                  (loop
-                    :for ,%n :from 0 :below ,num-of-defs
-                    :for ,%testdef := (nth ,%n ,%testdefs)
-                    :do (setf (aref ,%testcases ,%n)
-                              (make-testcase :input (getf ,%testdef :input)
-                                             :expected (getf ,%testdef :expected))))
-                  ;; running tests
-                  (loop
-                    :for ,%n :from 0 :below ,num-of-defs
-                    :for ,%testcase := (aref ,%testcases ,%n)
-                    :for ,%input := (let ((,%input (testcase-input ,%testcase)))
-                                      (if (atom ,%input)
-                                          (list ,%input)
-                                          ,%input))
-                    :for ,%actual := (apply ,%subject ,%input)
-                    :do (setf (testcase-actual ,%testcase)
-                              ,%actual)
-                    :do (setf (testcase-result ,%testcase)
-                              (equalp ,%actual (testcase-expected ,%testcase))))
-                  ;; collect results
-                  (loop
-                    :for ,%n :from 0 :below ,num-of-defs
-                    :for ,%testcase := (aref ,%testcases ,%n)
-                    :unless (testcase-result ,%testcase)
-                    :do (push ,%testcase ,%failed))
-                  ,%testcases)))))
+                      (,%testcases (prepare-testcases ',testdefs)))
+                  (run-testcases ,%subject ,%testcases)
+                  (collect-result ,%testcases))))))
         (t (error "a test context ~s is not implemented yet" context))))
