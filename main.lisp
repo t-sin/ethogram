a0058722f0ef4cc135a3030749c2332ab0abbd17
t-sin <shinichi.tanaka45@gmail.com>
2023-10-14 21:44:39 +0900

    Rename: tests -> catalogues, testcases -> patterns

--- main.lisp
+++ main.lisp
@@ -5,10 +5,10 @@
 
 (defparameter *catalogues* ())
 
-(defstruct test
+(defstruct catalogue
   name subject cases)
 
-(defstruct testcase
+(defstruct pattern
   input expected actual succeeded-p)
 
 (defun valid-context-p (subject)
@@ -29,15 +29,15 @@
                 :collect (list :input input :expected expected))))
     (nreverse defs)))
 
-(defun prepare-testcases (defs)
-  (let ((testcases (make-array (length defs))))
+(defun prepare-patterns (defs)
+  (let ((patterns (make-array (length defs))))
     (loop
       :for n :from 0 :below (length defs)
       :for def := (elt defs n)
-      :do (setf (elt testcases n)
-                (make-testcase :input (getf def :input)
-                               :expected (getf def :expected))))
-    testcases))
+      :do (setf (elt patterns n)
+                (make-pattern :input (getf def :input)
+                              :expected (getf def :expected))))
+    patterns))
 
 (defun prepare (subject defexp)
   (multiple-value-bind (validp msg)
@@ -45,10 +45,10 @@
     (unless validp (error msg)))
   (let* ((name (generate-name subject))
          (testdefs (parse-defs defexp))
-         (testcases (prepare-testcases testdefs))
-         (test (make-test :name name
-                          :subject subject
-                          :cases testcases)))
+         (patterns (prepare-patterns testdefs))
+         (test (make-catalogue :name name
+                               :subject subject
+                               :cases patterns)))
     (push test *catalogues*)
     test))
 
@@ -63,43 +63,43 @@
                 (prepare ,$subject ',defexp))))
           (t (error "a test context ~s is not implemented yet" context)))))
 
-(defun run-testcases (subject testcases)
+(defun run-patterns (subject patterns)
   (loop
-    :for n :from 0 :below (length testcases)
-    :for testcase := (elt testcases n)
-    :for input := (let ((input (testcase-input testcase)))
+    :for n :from 0 :below (length patterns)
+    :for pattern := (elt patterns n)
+    :for input := (let ((input (pattern-input pattern)))
                     (if (atom input)
                         (list input)
                         input))
     :for actual := (apply subject input)
-    :do (setf (testcase-actual testcase) actual)
-    :do (let ((expected (testcase-expected testcase)))
-          (setf (testcase-succeeded-p testcase)
+    :do (setf (pattern-actual pattern) actual)
+    :do (let ((expected (pattern-expected pattern)))
+          (setf (pattern-succeeded-p pattern)
                 (equalp actual expected)))))
 
-(defun collect-result (testcases)
+(defun collect-result (patterns)
   (let ((failed ()))
     (loop
-      :for n :from 0 :below (length testcases)
-      :for testcase := (elt testcases n)
-      :unless (testcase-succeeded-p testcase)
-      :do (push testcase failed))
+      :for n :from 0 :below (length patterns)
+      :for pattern := (elt patterns n)
+      :unless (pattern-succeeded-p pattern)
+      :do (push pattern failed))
     (nreverse failed)))
 
 (defun report-result (subject failed)
   (loop
     :for n :from 0 :below (length failed)
-    :for testcase := (elt failed n)
+    :for pattern := (elt failed n)
     :do (format t "~a expected ~a but an actual ~a~%"
                 subject
-                (testcase-expected testcase)
-                (testcase-actual testcase))))
+                (pattern-expected pattern)
+                (pattern-actual pattern))))
 
 (defun validate (test &key report)
-  (let ((subject (test-subject test))
-        (testcases (test-cases test)))
-    (run-testcases subject testcases)
-    (let ((failed (collect-result testcases)))
+  (let ((subject (catalogue-subject test))
+        (patterns (catalogue-cases test)))
+    (run-patterns subject patterns)
+    (let ((failed (collect-result patterns)))
       (if report
           (report-result subject failed)
           failed))))
@@ -111,6 +111,6 @@
       :do (setf failed (nconc failed (validate test))))
     (if report
         (loop
-          :for testcase :in failed
+          :for pattern :in failed
           :do (report-result "<test name here>" failed))
         failed)))
