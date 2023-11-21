3e317e93e078e5c9bb40940ea8885c6231e4ff9b
t-sin <shinichi.tanaka45@gmail.com>
2023-11-21 19:30:13 +0900

    Add TODO and desgin memo

--- test/ethogram.lisp
+++ test/ethogram.lisp
@@ -3,6 +3,34 @@
         :ethogram))
 (in-package :ethogram.test)
 
+;; # DSL example
+;;
+;; (defspec #'oddp
+;;   :to "Check number's oddness"
+;;   (spec :about "first argument"
+;;         (examples
+;;          :when "the argument are odd numbers"
+;;          :return nil :for 0
+;;          :return nil :for 2
+;;          :return nil :for 10)
+;;         (examples
+;;          :when "the argument are odd numbers"
+;;          :expect t
+;;          :for 1
+;;          :for 3
+;;          :for 1001)
+;;         (examples
+;;          :when "the argument is zero"
+;;          :expect t :for 0)))
+
+;; # TODO
+;;
+;; - [x] 検査を実行する
+;; - [ ] 検査の前に:setupを実行する
+;; - [ ] 検査が失敗しても:teardownを実行する
+;; - [ ] 複数の検査を実行する
+;; - [ ] 検査の結果を収集する
+
 (defun test.checked? ()
   (let ((test (defspec #'checked?)))
     (assert (null (checked? test)))
