58e166c72e81c11a1acadbf4e82ed8a59514aaee
t-sin <shinichi.tanaka45@gmail.com>
2023-11-23 15:34:12 +0900

    Remove "-fn"

--- ethogram.lisp
+++ ethogram.lisp
@@ -8,13 +8,13 @@
 
 (defstruct spec
   subject
-  prepare-fn
+  prepare
   (prepared? nil)
   (checked? nil))
 
 (defun defspec (subject &key (prepare (lambda ())))
   (make-spec :subject subject
-             :prepare-fn prepare))
+             :prepare prepare))
 
 (defun prepared? (spec)
   (spec-prepared? spec))
@@ -25,7 +25,7 @@
 (defgeneric prepare (spec))
 (defmethod prepare ((spec spec))
   (setf (spec-checked? spec) nil)
-  (funcall (spec-prepare-fn spec))
+  (funcall (spec-prepare spec))
   (setf (spec-prepared? spec) t))
 
 (defgeneric check (spec))
